9ac7b08a18e89401b75a0fbe02289c230b6a6597
saram88 <127104599+saram88@users.noreply.github.com>
2023-06-21 10:41:43 +0000

    save all files

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board=[]
+
+for x in range(5):
+    board.append(["0"] *5)
+
+def print_board(board):
+    for row in board:
+        print(" ",join(row))
+
+print("Let´s play!")
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) -1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+print(ship_row)
+print(ship_col)
+
+for turn in range(4):
+    print("Turn", turn + 1)
+    guess_row= int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
+
+if guess_row == ship_row and guess_col == ship_col:
+        print("Well done! You sank my battleship!")
+break
+
+else:
+if guess_row not in range(5) or guess_col not in range(5):
+            print("Oops, that's not even in the ocean.")
+        elif board[guess_row][guess_col] == "X":
+            print("You have already guess that one.")
+
+            else:
+            print("AHaha you missed my battleship!")
+            board[guess_row][guess_col] = "X"
+        if turn == 3:
+            print("Game Over")
+        print_board(board)
